29941a6e90b3f937a877c6781320988c096900f8
Iron <scriptok@bk.ru>
2012-02-26 22:42:02 +0400

    Create point

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prototype::Application.config.secret_token = '8d7acba338a11be671174d829aad5c3ce88bdf3e917ca9d3d4b974843d52e59a88174ae3e74e81f3b9618ee5e1e8a38ff8d62ee696f6a671371db1742daa4efd'
